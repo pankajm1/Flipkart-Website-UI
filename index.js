c14023650b63b27986d88dd389173c23f3415d40
pankajm1 <88309055+pankajm1@users.noreply.github.com>
2023-07-06 10:42:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const slides = document.querySelectorAll(".slide")
+var counter = 0;
+
+slides.forEach(
+    (slide,index) => {
+        slide.style.left =`${index * 100}%`
+    }
+)
+
+
+const goPrev = () => {
+    counter--
+    slideImage()
+}
+const goNext = () => {
+    counter++
+    slideImage()
+}
+const slideImage = () => {
+    slides.forEach(
+            (slide) => {
+                slide.style.transform =`translateX(-${counter *100}%)`
+                if (counter > 6){
+                    counter = 1;
+                }
+            }
+    )
+   
+}
+
+
+
+
